ed960f8075846d564539c22d4f6d7a8e317b7ffe
mitya-me <dmitrymefodyev@gmail.com>
2022-06-06 22:23:29 +0300

    Add: first version sof the slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,22 @@
 import { FC } from 'react' 
+import { Slider } from './component/slider/slider';
+import image from './component/slider/test_img.webp'
 
 const App:FC = () => { 
 
   return (
-    <div>
-      App component
+    <div style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: '100vh'
+    }}>
+      <Slider movies={[
+        {id: 1, title: 'Coffee and Cigaretes', description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, aut?', genre: 'Drama', image },
+        {id: 2, title: 'Graf Dracula', description: 'bla-bla-bla film', genre: 'Horror', image },
+        {id: 3, title: 'X-men', description: 'Mutant film', genre: 'Fantastic, Adventure', image },
+        {id: 4, title: 'RAMBO', description: 'Its classic action! Its classic hero!', genre: 'Action, drama', image },
+      ]}/>
     </div>
   )
 } 
--- src/component/slider/slider.tsx
+++ src/component/slider/slider.tsx
@@ -1,28 +1,57 @@
-import { FC, useState } from 'react' 
-import { ISliderProps } from './slider.props'
-import styles from './slider.module.scss'
-import cn from 'classnames'
-import {ReactComponent as Arrow} from './icons/arrow.svg'
+import { FC, useState } from 'react';
+import { ISliderProps } from './slider.props';
+import styles from './slider.module.scss';
+import cn from 'classnames';
+import { ReactComponent as Arrow } from './icons/arrow.svg';
 
-export const Slider = ({movies}:ISliderProps) => { 
-    const [slide, setSlide] = useState<number>(0)
+const fadeDuration = 300
+
+export const Slider = ({ movies }: ISliderProps) => {
+    const [slide, setSlide] = useState<number>(0);
+    const [fadeState, setFadeState] = useState<'fade-in' | 'fade-out'>('fade-in');
+    const [currentTimer, setCurrentTimer] = useState<NodeJS.Timeout>()
+
+    const handlerClick = (move: number) => {
+        const timer = setTimeout(() => {
+            setSlide(s => s + move)
+            setFadeState('fade-in')
+        }, 300)
+
+        clearTimeout(currentTimer)
+        setFadeState('fade-out')
+        setCurrentTimer(timer)
+    };
 
     return (
         <div className={styles.slider}>
-            <div className={styles.slide}>
-                <div className={styles.slide__left}>
-                    <div className={styles.slide__left_text}>{movies[slide].description}</div>
-                    <div className={styles.slide__left_title}>{movies[slide].title}</div>
-                    <div className={styles.slide__left_genre}>{movies[slide].genre}</div>
+            <div className={cn(styles.slide, styles[fadeState])}>
+                <div className={styles.slide__text}>
+                    <div className={styles.slide__text_title}>{movies[slide].title}</div>
+                    <div className={styles.slide__text_description}>
+                        {movies[slide].description}
+                    </div>
+                    <div className={styles.slide__text_genre}>{movies[slide].genre}</div>
                 </div>
-                <div className={styles.sliderRight} style={{backgroundImage: `url(${movies[slide].image})`}}/>
+                <div
+                    className={styles.slide__img}
+                    style={{ backgroundImage: `url(${movies[slide].image})` }}
+                />
             </div>
-            <div className={cn(styles.arrow, styles['arrow--left'])}> 
-                <Arrow/>
-            </div>
-            <div className={cn(styles.arrow, styles['arrow--right'])}> 
-                <Arrow/>
-            </div>
+
+            {slide > 0 && (
+                <button
+                    onClick={() => handlerClick(-1)}
+                    className={cn(styles.arrow, styles['arrow--left'])}
+                >
+                    <Arrow />
+                </button>
+            )}
+            {slide < movies.length - 1 && <button
+                onClick={() => handlerClick(1)}
+                className={cn(styles.arrow, styles['arrow--right'])}
+            >
+                <Arrow />
+            </button>}
         </div>
     );
-} 
+};
