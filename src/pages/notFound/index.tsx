91590e92d67844e1dde596b28e97ba453ab02c59
mitya-me <dmitrymefodyev@gmail.com>
2022-06-16 23:38:39 +0300

    Add: notFound page

--- src/pages/notFound/index.tsx
+++ src/pages/notFound/index.tsx
@@ -1,10 +1,17 @@
-
+import styles from './notFound.module.scss'
+import {UIButton} from '../../ui/uiButton'
 
 export const NotFoundPage = () => { 
 
     return (
-        <div>
-            NOT_FOUND_PAGE
+        <div className={styles.notFound}>
+            <div className={styles.notFound__gif}></div>
+            <h1 className={styles.notFound__title}> Lights, Camera, Oops - page not found! </h1>
+            <div className={styles.notFound__btns}>
+                <UIButton text='go to gome page' width='33%'/>
+                <UIButton text='show what movies are showing' width='33%'/>
+                <UIButton text='what events are happening' width='33%'/>
+            </div>
         </div>
     )
 } 
