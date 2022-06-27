76c191b85630db1984c7ddf21c1979def6fa14ea
mitya-me <dmitrymefodyev@gmail.com>
2022-06-27 13:22:31 +0300

    Refact: global styles and uiButton props

--- src/ui/uiButton/index.tsx
+++ src/ui/uiButton/index.tsx
@@ -2,9 +2,9 @@ import { IUiButtonProps } from "./uiButton.props";
 import styles from './uiButton.module.scss';
 import cn from 'classnames'
 
-export const UIButton = ({text, width = '45%', className}:IUiButtonProps) => { 
+export const UIButton = ({text, width = '45%', className, ...props}:IUiButtonProps) => { 
     return (
-        <button className={cn(styles.button, className)} style={{width: width}}>
+        <button className={cn(styles.button, className)} style={{width: width}} {...props}>
             {text}
         </button>
     )
